7347cedaa85dfed3c33716a068696ea83d828dda
Jasmina <Jasmina@JasminaPC>
2016-08-02 21:15:35 +0200

    Commit

--- src/zadaci_02_08_2016/LeapYear.java
+++ src/zadaci_02_08_2016/LeapYear.java
@@ -1,23 +1,31 @@
 package zadaci_02_08_2016;
 
+/* Napisati program koji ispisuje sve prestupne godine, 10 po liniji,
+ * od godine 101 do 2100, odvojene jednim spaceom.
+ * Takodjer ispisati i ukupan broj prijestupnih godina u ovom vremenskom periodu.
+ * 
+ * @author ortonka
+*/
+
 public class LeapYear {
 
 	public static void main(String[] args) {
-		//brojac za provjeru je li ispisano 10 godina u liniji
+		// brojac za provjeru je li ispisano 10 godina u liniji
 		int brojac = 0;
-		//brojac prijestupnih godina
+		// brojac prijestupnih godina
 		int brojac2 = 0;
-		
+
 		int godina1 = 101;
 		int godina2 = 2100;
-		//petlja sa uslovima koja provjerava i ispisuje da li je godina prijestupna
+		// petlja sa uslovima koja provjerava i ispisuje da li je godina
+		// prijestupna
 		for (int godina = godina1; godina < godina2; godina++) {
 			if (((godina % 4 == 0) && (godina % 100 != 0)) || (godina % 400 == 0)) {
 
 				System.out.print(godina + " ");
 				brojac++;
 				brojac2++;
-				//ukoliko je ispisano vec 10 godina u liniji, resetujemo brojac
+				// ukoliko je ispisano vec 10 godina u liniji, resetujemo brojac
 				if (brojac == 10) {
 					System.out.println();
 					brojac = 0;
@@ -26,7 +34,7 @@ public class LeapYear {
 			}
 
 		}
-		//ispis ukupnog broja prijestupnih godina
+		// ispis ukupnog broja prijestupnih godina
 		System.out.println("\nBroj prijestupnih godina u intervalu od 101. godine do 2100. je " + brojac2);
 	}
 
