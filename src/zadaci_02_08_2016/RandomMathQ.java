1171a1399faf6ddfef359146acb1556091cf8604
Jasmina <Jasmina@JasminaPC>
2016-08-03 19:19:34 +0200

    Commit

--- src/zadaci_02_08_2016/RandomMathQ.java
+++ src/zadaci_02_08_2016/RandomMathQ.java
@@ -14,11 +14,13 @@ import java.util.InputMismatchException;
 public class RandomMathQ {
 
 	public static void main(String[] args) {
-
+		//kreiranje skenera
 		java.util.Scanner input = new java.util.Scanner(System.in);
 
 		System.out.print("Unesite broj pitanja: ");
+		//varijabla za provjeru
 		boolean error = true;
+		//varijabla u koju se sprema broj pitanja
 		int numOfQ = 0;
 		do {
 			try {
@@ -29,12 +31,15 @@ public class RandomMathQ {
 				input.nextLine();
 			}
 		} while (error);
+		//varijabla za brojanje broja tacnih odgovora
 		int correct = 0;
 		for (int i = 0; i < numOfQ; i++) {
-
+			//2 random broja koja ce se koristiti u pitanjima
 			int num1 = (int) (Math.random() * 10);
 			int num2 = (int) (Math.random() * 10);
+			//tacan odgovor
 			int suma = 0;
+			//korisnikov odgovor
 			int sumaUser = 0;
 
 			boolean error2 = true;
@@ -55,7 +60,7 @@ public class RandomMathQ {
 					input.nextLine();
 				}
 			} while (error2);
-
+			//provjera da li je odgovor tacan
 			if (suma == sumaUser) {
 				correct++;
 				System.out.println("Cestitamo, odgovor je tacan.");
@@ -65,7 +70,7 @@ public class RandomMathQ {
 			}
 
 		}
-
+		//krajnji ispis
 		System.out.println("Od ukupnog broja od " + numOfQ + " pitanja, tacno je odgovorenih " + correct
 				+ ", a netacno odgovorenih je " + (numOfQ - correct));
 		input.close();
