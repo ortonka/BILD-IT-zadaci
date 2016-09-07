d2f3e190db1a242ce0312a3b9f7300f38e2f7838
Jasmina <Jasmina@JasminaPC>
2016-09-07 23:32:16 +0200

    Zadaci_06_09_2016

--- src/zadaci_06_09_2016/GreatestCommonDivisor.java
+++ src/zadaci_06_09_2016/GreatestCommonDivisor.java
@@ -5,7 +5,7 @@ public class GreatestCommonDivisor {
 	public static int gcd(int m, int n) {
 
 		if (m % n == 0)
-			return n;
+			return Math.abs(n);
 		else
 			return gcd(n, m % n);
 	}
@@ -13,10 +13,10 @@ public class GreatestCommonDivisor {
 	public static void main(String[] args) {
 		try (java.util.Scanner input = new java.util.Scanner(System.in)) {
 			System.out.print("Unesite prvu vrijednost: ");
-			int m = Math.abs(input.nextInt());
+			int m = input.nextInt();
 			System.out.print("Unesite drugu vrijednost: ");
-			int n = Math.abs(input.nextInt());
-			System.out.println("Najveci zajednicki djelilac brojeva '" + m + "' i '" + n + "' je '" + gcd(m, n) +"'.");
+			int n = input.nextInt();
+			System.out.println("Najveci zajednicki djelilac brojeva '" + m + "' i '" + n + "' je '" + gcd(m, n) + "'.");
 
 		} catch (Exception e) {
 			System.err.println("Dogodila se greska, pokusajte ponovo.");
